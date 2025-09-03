2a70f58f2e5dfe7548991ecad5018fd76843d309
sippo <sippo@DESKTOP-4013P7G>
2025-09-03 13:08:30 -0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MVCFirstApp.Models;
+
+namespace MVCFirstApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            
+                return View();
+        }
+
+        public IActionResult cit218()
+        {
+          
+                return View();
+        }
+
+        //GET: Home
+        public string myIndex()
+        {
+            return "Jerald's Awesome Index";
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
